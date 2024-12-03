5ff3e74e36ac86863988c989af4601536065150b
Maciej Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2024-12-03 21:17:38 +0100

    old changes

--- ESP32/ESPSerial/ESPSerial.ino
+++ ESP32/ESPSerial/ESPSerial.ino
@@ -1,211 +1,71 @@
-#define HOVER_SERIAL_BAUD 115200 // [-] Baud rate for HoverSerial (used to communicate with the hoverboard)
-#define SERIAL_BAUD 115200       // [-] Baud rate for built-in Serial (used for the Serial Monitor)
-#define START_FRAME 0xABCD       // [-] Start frme definition for reliable serial communication
-#define TIME_SEND 1              // [ms] Sending time interval
-#define SPEED_MAX_TEST 50        // [-] Maximum speed for testing
-#define SPEED_STEP 1             // [-] Speed step
-#define LED_BUILTIN 2            // [-] Built-in LED pin
-// #define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
+/*
+  Blink without Delay
 
-unsigned long StartTime = millis();
+  Turns on and off a light emitting diode (LED) connected to a digital pin,
+  without using the delay() function. This means that other code can run at the
+  same time without being interrupted by the LED code.
 
-uint16_t fps = 0;
-uint16_t cmdCount = 0;
-uint16_t lastCmdCount = 0;
+  The circuit:
+  - Use the onboard LED.
+  - Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA
+    and ZERO it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN
+    is set to the correct LED pin independent of which board is used.
+    If you want to know what pin the on-board LED is connected to on your
+    Arduino model, check the Technical Specs of your board at:
+    https://www.arduino.cc/en/Main/Products
 
+  created 2005
+  by David A. Mellis
+  modified 8 Feb 2010
+  by Paul Stoffregen
+  modified 11 Nov 2013
+  by Scott Fitzgerald
+  modified 9 Jan 2017
+  by Arturo Guadalupi
 
-// Global variables
-uint8_t idx = 0;        // Index for new data pointer
-uint16_t bufStartFrame; // Buffer Start Frame
-byte *p;                // Pointer declaration for the new received data
-byte incomingByte;
-byte incomingBytePrev;
+  This example code is in the public domain.
 
-typedef struct
-{
-  uint16_t start;
-  int16_t steer;
-  int16_t speed;
-  uint16_t checksum;
-} SerialCommand;
-SerialCommand Command;
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/BlinkWithoutDelay
+*/
 
-typedef struct
-{
-  uint16_t start;
-  int16_t cmd1;
-  int16_t cmd2;
-  int16_t speedR_meas;
-  int16_t speedL_meas;
-  int16_t batVoltage;
-  int16_t boardTemp;
-  uint16_t cmdLed;
-  uint16_t checksum;
-} SerialFeedback;
-SerialFeedback Feedback;
-SerialFeedback NewFeedback;
+// constants won't change. Used here to set a pin number:
+const int ledPin = 2;  // the number of the LED pin
 
-typedef struct
-{
-  uint16_t start;
-  int16_t cmd1;
-  int16_t cmd2;
-  int16_t speedR_meas;
-  int16_t speedL_meas;
-  int16_t batVoltage;
-  int16_t boardTemp;
-  uint16_t cmdLed;
-  uint16_t checksum;
-} SerialFeedback2;
-SerialFeedback Feedback2;
-SerialFeedback NewFeedback2;
+// Variables will change:
+int ledState = LOW;  // ledState used to set the LED
 
-#include <HardwareSerial.h>
-#define RX_PIN 16
-#define TX_PIN 17
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;  // will store last time LED was updated
 
-// Set up a new SoftwareSerial object
-// HardwareSerial Serial1(1);  // RX, TX
+// constants won't change:
+const long interval = 300;  // interval at which to blink (milliseconds)
 
-// ########################## SETUP ##########################
-void setup()
-{
-  Serial.begin(SERIAL_BAUD);
-  Serial.println("Hoverboard Serial v1.44");
-  pinMode(RX_PIN, INPUT);
-  pinMode(TX_PIN, OUTPUT);
-  Serial1.begin(HOVER_SERIAL_BAUD, SERIAL_8N1, RX_PIN, TX_PIN);
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
 }
 
-// ########################## SEND ##########################
-void SendFront(int16_t uSteer, int16_t uSpeed)
-{
-  Command.start = (uint16_t)START_FRAME;
-  Command.steer = (int16_t)uSteer;
-  Command.speed = 0;
-  Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
-  // if (Serial1.available()) {
-  //   // Serial1.write((uint8_t *)&Command, sizeof(Command));
-  // }
-  Serial1.write((uint8_t *)&Command, sizeof(Command));
-}
+void loop() {
+  // here is where you'd put code that needs to be running all the time.
 
-// ########################## RECEIVE ##########################
-void Receive()
-{
-  // Check for new data availability in the Serial buffer
-  if (Serial1.available())
-  {
-    incomingByte = Serial1.read();                                      // Read the incoming byte
-    bufStartFrame = ((uint16_t)(incomingByte) << 8) | incomingBytePrev; // Construct the start frame
-  }
-  else
-  {
-    return;
-  }
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
 
-// If DEBUG_RX is defined print all incoming bytes
-#ifdef DEBUG_RX
-  Serial.print(incomingByte);
-  return;
-#endif
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
 
-  // Copy received data
-  if (bufStartFrame == START_FRAME)
-  { // Initialize if new data is detected
-    p = (byte *)&NewFeedback;
-    *p++ = incomingBytePrev;
-    *p++ = incomingByte;
-    idx = 2;
-  }
-  else if (idx >= 2 && idx < sizeof(SerialFeedback))
-  { // Save the new received data
-    *p++ = incomingByte;
-    idx++;
-  }
-
-  // Check if we reached the end of the package
-  if (idx == sizeof(SerialFeedback))
-  {
-    uint16_t checksum;
-    checksum = (uint16_t)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
-
-    // Check validity of the new data
-    if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum)
-    {
-      // Copy the new data
-      memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
-
-      // Print data to built-in Serial
-      Serial.print("cmd1: ");
-      Serial.print(Feedback.cmd1);
-      Serial.print(" speedL_meas: ");
-      Serial.print(Feedback.speedL_meas);
-      Serial.print(" vol: ");
-      Serial.print(Feedback.batVoltage);
-      Serial.print(" lastCmdCount: ");
-      Serial.print(lastCmdCount);
-      Serial.print(" fps: ");
-      Serial.println(fps);
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
     }
-    else
-    {
-      Serial.println("Non-valid data skipped");
-    }
-    idx = 0; // Reset the index (it prevents to enter in this if condition in the next cycle)
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
   }
-
-  // Update previous states
-  incomingBytePrev = incomingByte;
 }
-
-
-// ########################## LOOP ##########################
-
-unsigned long iTimeSend = 0;
-int iTest = 0;
-int iStep = SPEED_STEP;
-
-
-unsigned long previousMicros = 0;
-unsigned long interval = 1000000; // 1 second interval in microseconds
-unsigned int loopCounter = 0;
-
-void loop(void)
-{
-  unsigned long timeNow = micros();
-
-  loopCounter++;
-  if (timeNow - previousMicros >= interval)
-  {
-    fps = loopCounter;
-    loopCounter = 0;
-    previousMicros = timeNow;
-    lastCmdCount = cmdCount;
-    cmdCount = 0;
-  }
-
-  // Check for new received data
-  Receive();
-  // delay(50);
-  // SendFront(30, 0);
-  // Send commands
-  if (iTimeSend > timeNow)
-    return;
-  iTimeSend = timeNow + 1000; // Set TIME_SEND to 200 microseconds
-  SendFront(8, 0);
-  cmdCount++;
-
-  // Calculate test command signal
-  iTest += iStep;
-
-  // invert step if reaching limit
-  if (iTest >= SPEED_MAX_TEST || iTest <= -SPEED_MAX_TEST)
-    iStep = -iStep;
-
-  // Blink the LED
-  digitalWrite(LED_BUILTIN, (timeNow % 2000) < 1000);  
-}
-
-// ########################## END ##########################
